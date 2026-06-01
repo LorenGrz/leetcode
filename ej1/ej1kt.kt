7f36d3c66489e2cfa91e8335bf483ca19f7541c5
LorenGrz <lorenzograizzaro55@gmail.com>
2026-06-01 17:21:26 -0300

    Agrego ej2

--- ej1/ej1kt.kt
+++ ej1/ej1kt.kt
@@ -3,15 +3,16 @@
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 You can return the answer in any order.*/
-
-fun twoSum(nums: IntArray, target: Int): IntArray {
-    val map = mutableMapOf<Int, Int>()
-    for (i in nums.indices) {
-        val complement = target - nums[i]
-        if (map.containsKey(complement)) {
-            return intArrayOf(map[complement]!!, i)
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val map = mutableMapOf<Int, Int>()
+        for (i in nums.indices) {
+            val complement = target - nums[i]
+            if (map.containsKey(complement)) {
+                return intArrayOf(map[complement]!!, i)
+            }
+            map[nums[i]] = i
         }
-        map[nums[i]] = i
+        throw IllegalArgumentException("No two sum solution")
     }
-    throw IllegalArgumentException("No two sum solution")
 }
