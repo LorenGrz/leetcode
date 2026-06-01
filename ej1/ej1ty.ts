24693342c1d3b221884ffbbf096564d7c1cfbdfd
LorenGrz <lorenzograizzaro55@gmail.com>
2026-06-01 12:33:49 -0300

    Jugando con LazyVim

--- ej1/ej1ty.ts
+++ ej1/ej1ty.ts
@@ -12,17 +12,11 @@ holad
 holad
 holad
 holad
+Holaaaaaaa hola hola chau
 holad
 holad
 holad
 holad
-holad
-holad
-holad
-holad
-holad
-holad
-holad
-holad
-holad
-
+If (function name(params:type) {
+  
+}
